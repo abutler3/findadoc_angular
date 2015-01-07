2d55157c6f0fccc1f45d5e20047fac5d2fe51afc
Andrew Butler <abutler3@gmail.com>
2015-01-07 16:28:31 -0600

    Adding more to JSON

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-var app = angular.module('findADoc', ['ngModal']);
+var app = angular.module('findADoc', ['ngModal', 'ui.utils']);
 
 app.config(function(ngModalDefaultsProvider) {
   return ngModalDefaultsProvider.set({
@@ -6,19 +6,38 @@ app.config(function(ngModalDefaultsProvider) {
   });
 });
 
-app.filter('unique', function() {
-    return function(input, key) {
-        var unique = {};
-        var uniqueList = [];
-        for(var i = 0; i < input.length; i++){
-            if(typeof unique[input[i][key]] == "undefined"){
-                unique[input[i][key]] = "";
-                uniqueList.push(input[i]);
-            }
+app.directive('errSrc', function() {
+  return {
+    link: function(scope, element, attrs) {
+
+      scope.$watch(function() {
+        return attrs['ngSrc'];
+      }, function (value) {
+        if (!value) {
+          element.attr('src', attrs.errSrc);
         }
-        return uniqueList;
-    };
+      });
+
+      element.bind('error', function() {
+        element.attr('src', attrs.errSrc);
+      });
+    }
+  }
 });
+
+// app.filter('unique', function() {
+//     return function(input, key) {
+//         var unique = {};
+//         var uniqueList = [];
+//         for(var i = 0; i < input.length; i++){
+//             if(typeof unique[input[i][key]] == "undefined"){
+//                 unique[input[i][key]] = "";
+//                 uniqueList.push(input[i]);
+//             }
+//         }
+//         return uniqueList;
+//     };
+// });
 
 app.controller('doctorsController', ['$scope', '$http', function($scope, $http) {
   $scope.link = "http://google.com";
@@ -26,7 +45,10 @@ app.controller('doctorsController', ['$scope', '$http', function($scope, $http) 
   $scope.toggleModal = function() {
     $scope.modalShown = !$scope.modalShown;
   };
-  $http.get('css/findadoc.json').success(function(data) {
-    $scope.doctors = data;
+  $http.get('css/merged1-20.json').success(function(data, status, headers, config) {
+    $scope.doctors = data.Provider;
+  }).
+  error(function(data, status, headers, confid) {
+    console.log("Building could not read JSON");
   });
 }]);
